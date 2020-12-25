bcc28099febbe79d7f10bbbbd988cefda1639510
qyf <emoqyf@163.com>
2020-12-25 23:14:31 +0800

    gzhipin api

---
+++ src/main/java/com/gzhipin/GzhipinApplication.java
@@ -0,0 +1,14 @@
+package com.gzhipin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GzhipinApplication {
+
+    public static void main(String[] args) {
+        System.out.println("a");
+        SpringApplication.run(GzhipinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gzhipin/GzhipinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzhipin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GzhipinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
